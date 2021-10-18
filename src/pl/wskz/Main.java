73852594ede8f0b0dce39595aa9a14096958dcbd
MikiKru <michal_kruczkowski@o2.pl>
2021-10-18 08:55:17 +0200

    Add new line into project

--- src/pl/wskz/Main.java
+++ src/pl/wskz/Main.java
@@ -3,5 +3,6 @@ package pl.wskz;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello in the Intenet Techniques");
+        System.out.println("Let's we start!!!");
     }
 }
